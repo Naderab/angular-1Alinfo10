80533712e749e95c4294f2c8116dc59a57df23bb
Naderab <abdellaouinader@gmail.com>
2023-05-24 17:46:33 +0100

    atelier2-exercice1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
+import { NotFoundComponent } from './models/not-found/not-found.component';
+import { DetailsProductComponent } from './models/details-product/details-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"products",pathMatch:"full"},
+  {path:"products",component:ProductsComponent},
+  {path:"product/:id",component:DetailsProductComponent},
+  {path:"offres",component:OffresEmploiComponent},
+  {path:"**",component:NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductsComponent } from './products/products.component';
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
+import { NotFoundComponent } from './models/not-found/not-found.component';
+import { DetailsProductComponent } from './models/details-product/details-product.component';
 
+ 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ProductsComponent
+    ProductsComponent,
+    OffresEmploiComponent,
+    NotFoundComponent,
+    DetailsProductComponent
   ],
   imports: [
     BrowserModule,

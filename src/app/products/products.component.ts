1d99aee1055d2f8f56a92d5fb0b86892acad2abb
Naderab <abdellaouinader@gmail.com>
2023-05-24 09:05:23 +0100

    databinding + bootstrap

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  listProducts:Product[]=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.listProducts=[
+    {id: "1", title: "T-shirt 1", price: 18, quantity: 0, like: 0},
+    {id: "2", title: "T-shirt 2", price: 21, quantity: 10, like: 0},
+    {id: "3", title: "T-shirt 3", price: 16, quantity: 8, like: 0},
+  ]
+  }
+  buy(p:Product){
+    p.quantity--;
   }
 
 }
